cc63857325a790cd2dddbda7493362ca38c217b6
70L <henkezcsgo@gmail.com>
2022-10-24 20:31:04 +0200

    Update footer.inc.php

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -12,6 +12,6 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
         integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
         crossorigin="anonymous"></script>
-<script src="<?= (BASE_PATH); ?>bootstrap/js/bootstrap.min.js"></script>
+<script src="<?= (BASE_PATH); ?>assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
